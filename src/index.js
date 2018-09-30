4e4239cbe2c770fb45586dbc0ff5523282118a7c
Crunkagator <briantheblack@gmail.com>
2018-10-01 00:05:53 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function getZerosCount(number) {
   let a = 1;
   let zc = 0;
   do{
-    zc+=Math.floor(number/Math.pow(5,a++));
-  } while (Math.pow(a,5) <= number)
+    zc+=Math.floor(number / (5 ** a++));
+  } while (5 ** a <= number)
   return zc;
 }
